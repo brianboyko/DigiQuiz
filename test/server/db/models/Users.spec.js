18f4d88acc989b6b6a4a7a03078fd6abbef513c1
Brian Boyko <brian.boyko@gmail.com>
2017-02-06 11:30:09 -0500

    More progress on Users tests

--- test/server/db/models/Users.spec.js
+++ test/server/db/models/Users.spec.js
@@ -17,6 +17,20 @@ const fakeUser = {
   last_name: "Faker"
 };
 
+const generateFake = (num) => {
+  let fakes = [];
+  for(let i = 0; i < num; i++){
+    fakes.push({
+      login: "L" + Math.random().toString(),
+      password: "P" + Math.random().toString(),
+      email: "E" + Math.random().toString(),
+      first_name: "F" + Math.random().toString(),
+      last_name: "N" + Math.random().toString(),
+    });
+  }
+  return fakes;
+};
+
 describe('Model: Users', function() {
   before(function(done) {
     // clear all items from the test DB.
@@ -56,14 +70,35 @@ describe('Model: Users', function() {
         .then((c) => parseInt(c[0].count)))
         .to.eventually.equal(1)
         .notify(done);
-    })
+    });
     it('should be the record we just entered', function(done) {
       expect(Users.read.by_login("faker")
         .then((r) => r[0])
         .then((r) => _.pick(r, ["login", "password", "email", "first_name", "last_name"])))
-        .to.eventually.eql(fakeUser).notify(done)
-    })
+        .to.eventually.eql(fakeUser).notify(done);
+    });
+
   });
+
+  describe('Users.read', function(){
+    let fakes;
+    before(function(done){
+      fakes = generateFake(5);
+      Promise.all(fakes.map((fake) => Users.create(fake))).then(() => done());
+    });
+    it('reads with a manually inserted by', function(done){
+      expect(Users.read('login')(fakes[0].login)
+        .then((r) => _.pick(r[0], ["login", "password", "email", "first_name", "last_name"])))
+        .to.eventually.eql(fakes[0]).notify(done);
+    });
+    it('reads .by_login', function(done) {
+      expect(Promise.all(fakes.map((fake) => Users.read.by_login(fake.login)))
+        .then((recs) => recs.map((rec) => rec[0]))
+        .then((recs) => recs.map((r) => _.pick(r, ["login", "password", "email", "first_name", "last_name"]))))
+        .to.eventually.eql(fakes).notify(done);
+    });
+  });
+
 
 
 });
