144a520687e4cce22d6afc5977ce87ce5ab39dbc
Brian Boyko <brian.boyko@gmail.com>
2017-02-13 05:24:52 -0500

    testing hydration of db

--- test/server/db/models/Joins.spec.js
+++ test/server/db/models/Joins.spec.js
@@ -41,247 +41,169 @@ Our database:
   Bob creates a deck "moo" with questions "what is cow" and "what is bull" and stats a game "cowgame"
     and invites Alice and Carol.
   Carol creates a deck "neigh" with question "what is horse" and invites Alice to "horsegame".
-  Calla creates a deck "baa" with question "what is sheep"and invites Bob. to "sheepgame"
+  Carol creates a deck "baa" with question "what is sheep"and invites Bob. to "sheepgame"
 */
 
 const hydrateDB = () => {
-  let store = {};
+    const EXAMPLE_USERS = ["Alice", "Bob", "Carol"]
 
-  const addUsersToDb = (usersNames) => new Promise((resolve, reject) => {
+    const writeAndGetBackWithId = (recordObj, model) => model.create(recordObj)
+      .then((recordId) => Object.assign(recordObj, {
+        id: recordId[0]
+      }));
 
-    Promise.all(usersNames.map((loginName) => {
-      store.users[loginName] = userCreate(loginName);
-      return Users.create(store.users[loginName])
-        .then((uid) => {
-          store.users[loginName].uid = uid[0];
-          return store.users[loginName];
-        })
+    const writeUserAndGetBackWithId = (userObj) => Users.create(userObj)
+      .then((userId) => Object.assign(userObj, {
+        uid: userId[0]
+      }));
 
-      }))
-      .then((usersAndIds) => resolve(usersAndIds))
+    const addUsersToDb = (usersNames) =>
+      Promise.all(usersNames.map((loginName) => writeUserAndGetBackWithId(userCreate(loginName))));
+
+    return addUsersToDb(EXAMPLE_USERS)
+      .then((usersRecords) => usersRecords.reduce((pv, uRecord) =>
+        Object.assign(pv, {
+          [uRecord.login]: uRecord
+        })), {})
+      .then((uRecordObj) => Promise.all([
+          uRecordObj,
+          Promise.all([
+            writeAndGetBackWithId({
+              created_by: uRecordObj.Alice.uid,
+              is_public: true,
+              title: "quack",
+              subject: "farm"
+            }, Decks),
+            writeAndGetBackWithId({
+              created_by: uRecordObj.Bob.uid,
+              is_public: true,
+              title: "moo",
+              subject: "farm"
+            }, Decks),
+            writeAndGetBackWithId({
+              created_by: uRecordObj.Carol.uid,
+              is_public: true,
+              title: "neigh",
+              subject: "farm",
+            }, Decks),
+            writeAndGetBackWithId({
+              created_by: uRecordObj.Carol.uid,
+              is_public: true,
+              title: "baa",
+              subject: "farm",
+            }, Decks)
+          ]),
+          Promise.all([
+            writeAndGetBackWithId({
+              created_by: uRecordObj.Alice.uid,
+              is_public: true,
+              type: "open",
+              category: "farm animals",
+              prompt: "What is Duck?",
+              choices: "",
+              answer: "bird",
+              point_value: 100,
+            }, Questions),
+            writeAndGetBackWithId({
+              created_by: uRecordObj.Bob.uid,
+              is_public: true,
+              type: "open",
+              category: "farm animals",
+              prompt: "What is Cow?",
+              choices: "",
+              answer: "mammal",
+              point_value: 100,
+            }, Questions),
+            writeAndGetBackWithId({
+              created_by: uRecordObj.Bob.uid,
+              is_public: true,
+              type: "open",
+              category: "farm animals",
+              prompt: "What is Bull?",
+              choices: "",
+              answer: "mammal",
+              point_value: 100,
+            }, Questions),
+            writeAndGetBackWithId({
+              created_by: uRecordObj.Bob.uid,
+              is_public: true,
+              type: "open",
+              category: "farm animals",
+              prompt: "What is Bull?",
+              choices: "",
+              answer: "mammal",
+              point_value: 100,
+            }, Questions),
+            writeAndGetBackWithId({
+              created_by: uRecordObj.Carol.uid,
+              is_public: true,
+              type: "open",
+              category: "farm animals",
+              prompt: "What is Horse?",
+              choices: "",
+              answer: "mammal",
+              point_value: 100,
+            }, Questions),
+            writeAndGetBackWithId({
+              created_by: uRecordObj.Bob.uid,
+              is_public: true,
+              type: "open",
+              category: "farm animals",
+              prompt: "What is sheep?",
+              choices: "",
+              answer: "mammal",
+              point_value: 100,
+            }, Questions)
+          ]),
+        ])
+        .then((resolutions) => ({
+          users: resolutions[0],
+          decks: resolutions[1].reduce((pv, dRecord) =>
+            Object.assign(pv, {
+              [dRecord.title]: dRecord
+            }), {}),
+          questions: resolutions[2].reduce((pv, qRecord) =>
+            Object.assign(pv, {
+              [qRecord.prompt]: qRecord
+            }), {}),
+        }))
+        .then((res) => Promise.all([
+          res,
+          Promise.all([writeAndGetBackWithId({
+              roomcode: "AAAA",
+              created_by: res.users.Alice.uid,
+              deck: res.decks.quack.id,
+            }, Games),
+            writeAndGetBackWithId({
+              roomcode: "BBBB",
+              created_by: res.users.Bob.uid,
+              deck: res.decks.moo.id,
+            }, Games),
+            writeAndGetBackWithId({
+              roomcode: "CCCC",
+              created_by: res.users.Carol.uid,
+              deck: res.decks.neigh.id,
+            }, Games),
+            writeAndGetBackWithId({
+              roomcode: "DDDD",
+              created_by: res.users.Carol.uid,
+              deck: res.decks.baa.id,
+            }, Games)
+          ])
+        ]))
+        .then((resolutions) => Object.assign(resolutions[0], {
+          games: resolutions[1].reduce((pv, gRecord) =>
+            Object.assign(pv, { [gRecord.roomcode]: gRecord }), {})
+          })
+        ));
+
+};
+
+describe("foo", function(){
+  it('hydrates the DB', function(done){
+    hydrateDB().then((res) => {
+      console.log(res);
     })
-    /// use above, not below, need to go to dinner
-
-
-
-  return new Promise ((resolve, reject) => {
-    Promise.all(["Alice", "Bob", "Carol"].map((loginName) => {
-    store.users[loginName] = userCreate(loginName);
-    return Users.create(userCreate(loginName))
-      .then((uid) => {
-        store.users[loginName].uid = uid[0];
-        return store.users[loginName];
-      })
-  }
-    return Users.create(userCreate(loginName)))
-  }
-    .then((userIds) => userIds.map((id) => id[0]))
-    .then((uIds) => {
-
-      return  Promise.all(uIds.map((uid) => Users.read.by_uid(uid)))
-
-    })
-
-
-    .then((records) => Promise.all(records.map((record) => {
-      if(record.login === "Alice"){
-        return Decks.create({
-          created_by: record.uid,
-          is_public: true,
-          title: "quack",
-          subject: "farm"
-        });
-      } else if (record.login === "Bob"){
-        return Decks.create({
-          created_by: record.uid,
-          is_public: true,
-          title: "moo",
-          subject: "farm"
-        });
-      } else if (record.login === "Carol") {
-          return Promise.all([Decks.create({
-            created_by: record.uid,
-            is_public: true,
-            title: "neigh",
-            subject: "farm",
-          }),
-          Decks.create({
-            created_by: record.uid,
-            is_public: true,
-            title: "baa",
-            subject: "farm",
-          })]);
-      }
-    })));
-    .then((deckIds) => Promise.all())
-    // then do qestions next.
-
-
-
-
-
-}
-// describe('Model: Keywords', function() {
-//   before(function(done) {
-//     // clear all items from the test DB.
-//     clearDB(done);
-//   });
-//
-//   it('should start from a clear test database', function(done) {
-//     expect(Keywords.count()
-//         .then((c) => parseInt(c[0].count)))
-//       .to.eventually.equal(0)
-//       .notify(done);
-//   });
-//
-//   describe('Keywords.count()', function() {
-//     it('should count the keywords', function(done) {
-//       expect(Keywords.count()
-//           .then((c) => !isNaN(c[0].count)))
-//         .to.eventually.equal(true)
-//         .notify(done);
-//     });
-//   });
-//
-//   describe('Keywords.create()', function() {
-//     it('should create a keyword', function(done) {
-//       this.timeout(10000);
-//
-//       expect(Keywords.create({
-//             word: "Maths"
-//           })
-//           .then((idObj) => idObj[0]))
-//         .to.eventually.be.a('Number')
-//         .notify(done);
-//     });
-//     it('should confirm we have one record', function(done) {
-//       expect(Keywords.count()
-//           .then((c) => parseInt(c[0].count)))
-//         .to.eventually.equal(1)
-//         .notify(done);
-//     });
-//     it('should be the record we just entered', function(done) {
-//       expect(Keywords.read.by_word("Maths")
-//           .then((r) => r[0])
-//           .then((r) => r.hasOwnProperty('id') && !isNaN(r.id) && r.word === "Maths"))
-//         .to.eventually.equal(true)
-//         .notify(done);
-//     });
-//   });
-//
-//   describe('Keywords.read', function() {
-//     before(function(done) {
-//       clearDB(() => {
-//         Promise.all(["Science", "Technology", "Engineering", "Arts", "Mathmatics"].map((subj) => Keywords.create({
-//           word: subj
-//         }))).then(() => done());
-//       });
-//     });
-//     it('reads with a manually inserted by', function(done) {
-//       expect(Keywords.read('word')("Technology")
-//           .then((r) => r[0])
-//           .then((r) => r.hasOwnProperty('id') && !isNaN(r.id) && r.word === "Technology"))
-//         .to.eventually.equal(true)
-//         .notify(done);
-//     });
-//     it('reads .by_word', function(done) {
-//       expect(Keywords.read.by_word("Arts")
-//           .then((r) => r[0])
-//           .then((r) => r.hasOwnProperty('id') && !isNaN(r.id) && r.word === "Arts"))
-//         .to.eventually.equal(true)
-//         .notify(done);
-//     });
-//     it('reads .all', function(done) {
-//       expect(Keywords.read.all())
-//         .to.eventually.be.an('Array')
-//         .with.length(5)
-//         .notify(done);
-//     });
-//   });
-//
-//   describe('Keywords.update', function() {
-//     before(function(done) {
-//       clearDB(() => {
-//         Promise.all(["Science", "Technology", "Engineering", "Arts", "Mathmatics"].map((subj) => Keywords.create({
-//             word: subj
-//           })))
-//           .then(() => done());
-//       });
-//     });
-//     it('updates by arbitrary convention', function(done) {
-//       expect(Keywords.update('word')("Mathmatics", {
-//             word: "Maths"
-//           })
-//           .then(() => Keywords.read.by_word('Maths'))
-//           .then((r) => {
-//             r = r[0];
-//             let a = r.hasOwnProperty('id');
-//             let b = !isNaN(r.id);
-//             let c = r.word === "Maths";
-//             return a && b && c;
-//           }))
-//         .to.eventually.equal(true)
-//         .notify(done);
-//     });
-//     it('updates .by_id', function(done) {
-//       let myId;
-//       expect(Keywords.read.by_word('Arts')
-//           .then((records) => {
-//             myId = records[0].id;
-//             return myId;
-//           })
-//           .then((id) => Keywords.update.by_id(id, {
-//             word: "Arts & Crafts"
-//           })
-//           .then(() => Keywords.read.by_word("Arts & Crafts")))
-//           .then((records) => records[0].id === myId))
-//         .to.eventually.equal(true)
-//         .notify(done);
-//     });
-//   });
-//
-//   describe('Keywords.del', function() {
-//     before(function(done) {
-//       clearDB(() => {
-//         Promise.all(["Science", "Technology", "Engineering", "Arts", "Mathmatics"].map((subj) => Keywords.create({
-//             word: subj
-//           })))
-//           .then(() => done());
-//       });
-//     });
-//     it('deletes by arbitrary convention', function(done) {
-//       let before;
-//       expect(Keywords.count()
-//           .then((counts) => parseInt(counts[0].count))
-//           .then((numCount) => {
-//             before = numCount;
-//             return Keywords.del('word')('Science');
-//           })
-//           .then(() => Keywords.count())
-//           .then((counts) => parseInt(counts[0].count))
-//           .then((numCount) => before - numCount))
-//         .to.eventually.equal(1)
-//         .notify(done);
-//     });
-//     it('deletes .by_id', function(done) {
-//       let before;
-//       let myId;
-//       expect(Keywords.count()
-//           .then((counts) => parseInt(counts[0].count))
-//           .then((numCount) => {
-//             before = numCount;
-//             return Keywords.read.by_word("Technology");
-//           })
-//           .then((records) => {
-//             myId = records[0].id;
-//             return myId;
-//           })
-//           .then((id) => Keywords.del.by_id(id))
-//           .then(() => Keywords.count())
-//           .then((counts) => parseInt(counts[0].count))
-//           .then((numCount) => before - numCount))
-//         .to.eventually.equal(1)
-//         .notify(done);
-//     });
-//   });
-// });
+    .then(() => done());
+  });
+});
